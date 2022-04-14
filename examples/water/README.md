f106dc9430207114b4e75f4631ef6d793c0d5cbf
richard <yueyericardo@gmail.com>
2022-04-14 15:51:49 -0400

    water 15nm example

---
+++ examples/water/README.md
@@ -0,0 +1,9 @@
+Generate water box
+```bash
+python waterbox.py 15
+```
+
+Generate lammps data from pdb
+```bash
+python ../pdb2lmp.py water-15nm.pdb water-15nm.data
+```
---
+++ examples/water/waterbox.py
@@ -0,0 +1,20 @@
+import argparse
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from simtk import unit
+from openmmtools import testsystems
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('box_size', type=int, help="water box size in nanometer")
+    args = parser.parse_args()
+
+    box_size = args.box_size
+    water_box = testsystems.WaterBox(box_edge=box_size*unit.nanometer)
+    positions = water_box.positions
+    atoms = list(water_box.topology.atoms())
+    elements = [a.element.symbol for a in atoms]
+    pdbfile = f'water-{box_size}nm.pdb'
+    PDBFile.writeFile(water_box.topology, positions, open(pdbfile, 'w'))
+    print(f"check pdbfile: {pdbfile}")

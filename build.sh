07e10ec570ac3fabddfb72574f91aa531ebe060d
richard <yueyericardo@gmail.com>
2022-08-29 12:27:03 -0400

    quick save

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export LAMMPS_PLUGIN_PATH=${PWD}/build/
 
 # build lammps
 cd external/lammps/
-mkdir build-test; cd build-test
+mkdir -p build-test; cd build-test
 # D_GLIBCXX_USE_CXX11_ABI: https://stackoverflow.com/a/50873329/9581569
 cmake -DCMAKE_C_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0'  -DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' -DLAMMPS_INSTALL_RPATH=yes -DPKG_GPU=no \
 -DGPU_API=cuda -DGPU_ARCH=sm_80 -DPKG_PLUGIN=yes -DCMAKE_INSTALL_PREFIX=${HOME}/.local -DBUILD_MPI=yes -DBUILD_SHARED_LIBS=yes -DLAMMPS_MACHINE=mpi \
@@ -21,7 +21,7 @@ mpirun -np 1 ${lammps_root}/build-test/test_pair_style ../unittest/force-styles/
 
 # build lammps-ani
 cd ../../
-mkdir build; cd build
+mkdir -p build; cd build
 cmake -DCMAKE_C_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' -DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' -DLAMMPS_HEADER_DIR=${lammps_root}/src -DCUDNN_INCLUDE_PATH=${CONDA_PREFIX}/include -DCUDNN_LIBRARY_PATH=${CONDA_PREFIX}/lib ..
 make -j
 

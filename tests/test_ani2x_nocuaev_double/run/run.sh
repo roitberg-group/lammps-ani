4bd3401d6685b7ffb5e47cc1e4bef65f8de3829a
richard <yueyericardo@gmail.com>
2022-08-29 18:09:53 -0400

    quick save

--- tests/test_ani2x_nocuaev_double/run/run.sh
+++ tests/test_ani2x_nocuaev_double/run/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-mpirun -np 1 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
-mpirun -np 1 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
+mpirun -np 1 ../../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
+mpirun -np 1 ../../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
 
 ntasks=${SLURM_NTASKS:-1}
 if [ $ntasks -gt 1 ]; then
-    mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
-    mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
+    mpirun -np 2 ../../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
+    mpirun -np 2 ../../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
 fi
 
 rm log.lammps

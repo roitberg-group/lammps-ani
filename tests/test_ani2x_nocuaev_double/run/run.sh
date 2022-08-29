1f1dcbfadb783f62586be612d25ac45b1ddc6d32
richard <yueyericardo@gmail.com>
2022-08-29 18:08:58 -0400

    only run mpirun -np 2 when have enough ntasks

--- tests/test_ani2x_nocuaev_double/run/run.sh
+++ tests/test_ani2x_nocuaev_double/run/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ex
 
-# cuda
 mpirun -np 1 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
-mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
-# cpu
 mpirun -np 1 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
-mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
+
+ntasks=${SLURM_NTASKS:-1}
+if [ $ntasks -gt 1 ]; then
+    mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
+    mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
+fi
 
 rm log.lammps
 rm water.final

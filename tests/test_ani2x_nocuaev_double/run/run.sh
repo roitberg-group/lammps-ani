069c1c7bf71a3cba62122f2fa96e1c165123c8ce
richard <yueyericardo@gmail.com>
2022-08-29 17:55:28 -0400

    relative bin location

--- tests/test_ani2x_nocuaev_double/run/run.sh
+++ tests/test_ani2x_nocuaev_double/run/run.sh
@@ -2,11 +2,11 @@
 set -ex
 
 # cuda
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/lmp_mpi -in in.lammps.cuda
-mpirun -np 2 /blue/roitberg/apps/lammps/build-test/lmp_mpi -in in.lammps.cuda
+mpirun -np 1 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
+mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cuda
 # cpu
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/lmp_mpi -in in.lammps.cpu
-mpirun -np 2 /blue/roitberg/apps/lammps/build-test/lmp_mpi -in in.lammps.cpu
+mpirun -np 1 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
+mpirun -np 2 ../../external/lammps/build-test/lmp_mpi -in in.lammps.cpu
 
 rm log.lammps
 rm water.final
--- tests/test_ani2x_nocuaev_double/test.sh
+++ tests/test_ani2x_nocuaev_double/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/test_pair_style manybody-pair-ani-double-cpu.yaml -d ./ # -s
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/test_pair_style manybody-pair-ani-double-cuda.yaml -d ./ # -s
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/test_pair_style manybody-pair-ani-pbc-double-cpu.yaml -d ./ # -s
+mpirun -np 1 ../../external/lammps/build-test/test_pair_style manybody-pair-ani-double-cpu.yaml -d ./ # -s
+mpirun -np 1 ../../external/lammps/build-test/test_pair_style manybody-pair-ani-double-cuda.yaml -d ./ # -s
+mpirun -np 1 ../../external/lammps/build-test/test_pair_style manybody-pair-ani-pbc-double-cpu.yaml -d ./ # -s
 # when newton_bond is on, verlet.cpp calls reverse_comm() instead of our manual call
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/test_pair_style manybody-pair-ani-pbc-double-cpu-newton_bond-on.yaml -d ./ # -s
-mpirun -np 1 /blue/roitberg/apps/lammps/build-test/test_pair_style manybody-pair-ani-pbc-double-cuda.yaml -d ./ # -s
+mpirun -np 1 ../../external/lammps/build-test/test_pair_style manybody-pair-ani-pbc-double-cpu-newton_bond-on.yaml -d ./ # -s
+mpirun -np 1 ../../external/lammps/build-test/test_pair_style manybody-pair-ani-pbc-double-cuda.yaml -d ./ # -s
 

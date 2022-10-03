6bf1726b885972d6533f8883576c2a6e77931b7a
richard <yueyericardo@gmail.com>
2022-10-03 17:27:53 -0400

    compile kokkos separately

--- build.sh
+++ build.sh
@@ -16,33 +16,31 @@ python save_ani.py
 cd ../
 
 # build kokkos
-cd external/lammps/lib/kokkos
-rm -rf build; mkdir -p build; cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=~/.local/kokkos -DBUILD_SHARED_LIBS=yes -DKokkos_ENABLE_CUDA=yes -DKokkos_ENABLE_OPENMP=yes -DKokkos_ARCH_AMPERE80=yes -DKokkos_ENABLE_CUDA_LAMBDA=yes # -DKokkos_ENABLE_SERIAL=yes -DKokkos_ARCH_TURING75=yes
+cd external/lammps
+rm -rf build-kokkos; mkdir -p build-kokkos; cd build-kokkos
+cmake -DCMAKE_C_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${CXX11_ABI}" -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${CXX11_ABI}" \
+-DCMAKE_INSTALL_PREFIX=~/.local/kokkos -DBUILD_SHARED_LIBS=yes -DKokkos_ENABLE_CUDA=yes -DKokkos_ENABLE_OPENMP=yes -DKokkos_ENABLE_SERIAL=yes\
+-DKokkos_ARCH_AMPERE80=yes -DKokkos_ENABLE_CUDA_LAMBDA=yes ../lib/kokkos # -DKokkos_ARCH_TURING75=yes
 make -j
 make install
-cd ../../../../../
+cd ..
 
 # build lammps
-cd external/lammps/
-# a patch that allows us to build kokkos as a shared library
-# git apply ../lammps-kokkos.patch
 # kokkos flag
 KOKKOS_FLAGS="-DPKG_KOKKOS=yes -DEXTERNAL_KOKKOS=yes"
 rm -rf build; mkdir -p build; cd build
 # D_GLIBCXX_USE_CXX11_ABI: https://stackoverflow.com/a/50873329/9581569
 cmake -DCMAKE_C_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${CXX11_ABI}" -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${CXX11_ABI}" -DLAMMPS_INSTALL_RPATH=yes \
--DPKG_PLUGIN=yes -DCMAKE_INSTALL_PREFIX=${HOME}/.local/lammps -DBUILD_MPI=yes -DBUILD_SHARED_LIBS=yes -DLAMMPS_MACHINE=mpi \
--DPKG_EXTRA-PAIR=yes -DPKG_MOLECULE=yes -DPKG_OPENMP=yes -DENABLE_TESTING=yes -DLAMMPS_EXCEPTIONS=yes \
+-DCMAKE_INSTALL_PREFIX=${HOME}/.local/lammps -DCMAKE_PREFIX_PATH=${HOME}/.local \
+-DPKG_PLUGIN=yes -DBUILD_MPI=yes -DBUILD_SHARED_LIBS=yes -DLAMMPS_MACHINE=mpi \
+-DPKG_EXTRA-PAIR=yes -DPKG_MOLECULE=yes -DPKG_OPENMP=yes -DENABLE_TESTING=no -DLAMMPS_EXCEPTIONS=yes \
 $KOKKOS_FLAGS \
 ../cmake/
 make -j
 make install
-cd ../
-# git apply --reverse ../lammps-kokkos.patch
 # test
-mpirun -np 1 ${LAMMPS_ROOT}/build/test_pair_style ../unittest/force-styles/tests/mol-pair-morse.yaml
-cd ../../
+# mpirun -np 1 ${LAMMPS_ROOT}/build/test_pair_style ../unittest/force-styles/tests/mol-pair-morse.yaml
+cd ../../../
 
 # build lammps-ani
 source build-lammps-ani.sh

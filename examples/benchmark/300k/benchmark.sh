def184fd6d4d5c88abeb571cf3991f542635e294
richard <yueyericardo@gmail.com>
2022-10-05 08:54:09 -0400

    quick save

--- examples/benchmark/300k/benchmark.sh
+++ examples/benchmark/300k/benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-et -ex
+set -ex
 
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -in in.lammps -var num_models 1
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -in in.lammps -var num_models 4
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -in in.lammps -var num_models 8
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -pk kokkos newton on -in in.lammps -var num_models 1
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -pk kokkos newton on -in in.lammps -var num_models 4
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -pk kokkos newton on -in in.lammps -var num_models 8
+mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 1 -in in.lammps
+mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 4 -in in.lammps
+mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 8 -in in.lammps
+mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 1 -in in.lammps
+mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 4 -in in.lammps
+mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 8 -in in.lammps

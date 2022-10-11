87ad3d22e016029e88350602210bb9c255033c26
richard <yueyericardo@gmail.com>
2022-10-11 16:41:13 -0400

    use same in.lammps template for kokkos

--- tests/test_ani2x_cuaev_single_full_kokkos/run/run.sh
+++ tests/test_ani2x_cuaev_single_full_kokkos/run/run.sh
@@ -4,15 +4,15 @@ NUM_TASKS=${SLURM_NTASKS:-1}
 NUM_GPUs=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
 if [ $NUM_GPUs -gt 0 ]; then
-    mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -in in.lammps.cuda
-    mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -in in.lammps.cuda.kokkos
+    mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 8 -in in.lammps.cuda
+    LAMMPS_ANI_PROFILING=1 mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 8 -in in.lammps.cuda
 fi
 
 # Domain Decomposition with 2 processes
 if [ $NUM_TASKS -gt 1 ]; then
     if [ $NUM_GPUs -gt 0 ]; then
-        mpirun -np 2 ${LAMMPS_ROOT}/build/lmp_mpi -in in.lammps.cuda
-        mpirun -np 2 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 2 -sf kk -in in.lammps.cuda.kokkos
+        mpirun -np 2 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 8 -in in.lammps.cuda
+        LAMMPS_ANI_PROFILING=1 mpirun -np 2 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 2 -sf kk -var newton_pair on -var num_models 8 -in in.lammps.cuda
     fi
 fi
 

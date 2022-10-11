7a041767c14a60b967e3970ce033a05cf0bde9c2
Jinze (Richard) Xue <yueyericardo@gmail.com>
2022-10-11 13:02:18 -0400

    Update build-lammps-ani.sh

--- build-lammps-ani.sh
+++ build-lammps-ani.sh
@@ -7,7 +7,6 @@ source build-env.sh
 # copy source files to ani_csrc
 cp external/torchani_sandbox/torchani/csrc/* ani_csrc/
 
-# TODO we need to move this after lammps kokkos build
 # build lammps-ani
 rm -rf build; mkdir -p build; cd build
 # cmake -DCMAKE_C_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${CXX11_ABI}" -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=${CXX11_ABI}" -DLAMMPS_HEADER_DIR=${LAMMPS_ROOT}/src -DCUDNN_INCLUDE_PATH=${CONDA_PREFIX}/include -DCUDNN_LIBRARY_PATH=${CONDA_PREFIX}/lib ..

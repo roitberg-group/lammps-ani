d2db058d15aa36d0735f93943f0dd72b2c134320
richard <yueyericardo@gmail.com>
2023-01-09 14:52:46 -0500

    test_model.cpp, test_all.sh

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -4,11 +4,11 @@ set -ex
 NUM_GPUs=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
 # test_model
-${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_nocuaev_double_half.pt cpu
-${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_nocuaev_single_half.pt cpu
+${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_double.pt cpu
+${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_single.pt cpu
 if [ $NUM_GPUs -gt 0 ]; then
-    ${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_nocuaev_double_half.pt cuda
-    ${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_nocuaev_single_half.pt cuda
+    ${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_double.pt cuda
+    ${LAMMPS_ANI_ROOT}/build/ani_csrc/test_model ani2x_single.pt cuda
 fi
 
 # test_ani2x_nocuaev_double

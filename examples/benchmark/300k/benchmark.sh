798ee2bfb2083cd27b27f1f616b2608aefd2ae59
richard <yueyericardo@gmail.com>
2023-02-02 14:48:12 -0500

    Clean up 2

--- examples/benchmark/300k/benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 1 -in in.lammps
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 4 -in in.lammps
-mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -var num_models 8 -in in.lammps
-LAMMPS_ANI_PROFILING=1 mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 1 -in in.lammps
-LAMMPS_ANI_PROFILING=1 mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 4 -in in.lammps
-LAMMPS_ANI_PROFILING=1 mpirun -np 1 ${LAMMPS_ROOT}/build/lmp_mpi -k on g 1 -sf kk -var newton_pair on -var num_models 8 -in in.lammps
--- examples/water/README.md
+++ examples/water/README.md
@@ -1,9 +1,9 @@
 Generate water box
 ```bash
-python waterbox.py 5 
+python waterbox.py 6
 ```
 
 Generate lammps data from pdb
 ```bash
-python ../pdb2lmp.py water-5nm.pdb water-5nm.data
+python ../pdb2lmp.py water-6nm.pdb water-6nm.data --center
 ```

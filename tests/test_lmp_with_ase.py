5bcfcb03afb888071e5c2e5baaa75c60d473eb4a
richard <yueyericardo@gmail.com>
2023-02-02 11:31:28 -0500

    quick save

--- tests/test_lmp_with_ase.py
+++ tests/test_lmp_with_ase.py
@@ -2,10 +2,98 @@ import torch
 import torchani
 import os
 import pytest
-import lammps_ani
+import yaml
+import subprocess
+import numpy as np
+from typing import Dict
+from ase.io import read
+from ase.md.verlet import VelocityVerlet
+from ase.io.trajectory import Trajectory
+from ase import units
 import save_ani
 
 LAMMPS_PATH = os.path.join(os.environ["LAMMPS_ROOT"], "build/lmp_mpi")
+STEPS = 4
+
+
+class LammpsRunner():
+    def __init__(self, lmp: str, input_file: str, var_dict: Dict, kokkos: bool, num_tasks: int = 1):
+        var_dict["dump_file"] = "dump.yaml"
+        var_commands = " ".join([f"-var {var} {value}" for var, value in var_dict.items()])
+        kokkos_commands = f"-k on g {num_tasks} -sf kk" if kokkos else ""
+        run_commands = f"mpirun -np {num_tasks} {lmp} {var_commands} -var steps {STEPS} {kokkos_commands} -in {input_file}"
+        print(f"\n{run_commands}")
+        self.run_commands = run_commands
+        self.var_dict = var_dict
+
+    def run(self):
+        stdout = subprocess.run(self.run_commands, shell=True, check=True)
+        with open(self.var_dict["dump_file"], "r") as stream:
+            documents = list(yaml.safe_load_all(stream))
+        return documents
+
+
+class AseRunner():
+    def __init__(self, input_file: str, calculator, pbc: bool = False):
+        atoms = read(input_file)
+
+        print(len(atoms), "atoms in the cell")
+        atoms.set_calculator(calculator)
+        if not pbc:
+            atoms.set_pbc([False, False, False])
+
+        hartree2kcalmol = 627.5094738898777
+
+        def printenergy(a=atoms):
+            """Function to print the potential, kinetic and total energy."""
+            epot = a.get_potential_energy() / units.Hartree * hartree2kcalmol
+            ekin = a.get_kinetic_energy() / units.Hartree * hartree2kcalmol
+            # forces = atoms.get_forces().astype(np.double) / units.Hartree * hartree2kcalmol
+            print(
+                "Energy: Epot = %.13f kcal/mol  Ekin = %.13f kcal/mol (T=%3.2fK)  "
+                "Etot = %.13f kcal/mol"
+                % (epot, ekin, a.get_temperature(), epot + ekin)
+            )
+
+        dyn = VelocityVerlet(
+            atoms, timestep=0.1 * units.fs, trajectory="md.traj", logfile="md.log"
+        )
+        dyn.attach(printenergy, interval=1)
+        self.dyn = dyn
+
+    def run(self):
+        print("Beginning dynamics...")
+        self.dyn.run(STEPS)  # take 1000 steps
+        traj = list(Trajectory('md.traj'))
+        return traj
+
+
+def compare_lmp_ase(lmp_dump, ase_traj, high_prec):
+    # with open("tests/dump.yaml", "r") as stream:
+    #     lmp_dump = list(yaml.safe_load_all(stream))
+    # ase_traj = list(Trajectory('tests/md.traj'))
+    atol = 1e-9 if high_prec else 1e-3
+    rtol = 1e-5 if high_prec else 1e-3
+    num_traj = len(ase_traj)
+    for i in range(num_traj):
+        lmp_data = lmp_dump[i]
+        lmp_potEng = lmp_data["thermo"][1]["data"][1]
+        lmp_index = np.array(lmp_data["data"])[:, 0]
+        lmp_data["data"] = np.array(lmp_data["data"])[np.argsort(lmp_index)]
+        lmp_pos = np.array(lmp_data["data"])[:, 2:5]
+        lmp_force = np.array(lmp_data["data"])[:, 5:]
+
+        ase_atoms = ase_traj[i]
+        hartree2kcalmol = 627.5094738898777
+        ase_pos = ase_atoms.positions
+        ase_force = ase_atoms.get_forces() / units.Hartree * hartree2kcalmol
+        ase_potEng = ase_atoms.get_potential_energy() / units.Hartree * hartree2kcalmol
+        print("force error: ", np.abs(ase_force - lmp_force).max())
+        assert np.allclose(ase_force, lmp_force, rtol, atol)
+        assert np.allclose(ase_pos, lmp_pos, rtol, atol)
+        if i > 0:
+            assert np.allclose(lmp_potEng, ase_potEng, rtol, atol)
+
 
 pbc_params = [
     pytest.param(True, id="pbc_true"),
@@ -23,6 +111,7 @@ use_repulsion_params = [
     pytest.param(False, id="repulsion-no"),
     pytest.param(True, id="repulsion-yes"),
 ]
+
 
 @pytest.mark.parametrize("pbc", pbc_params)
 @pytest.mark.parametrize("precision", precision_params)
@@ -66,7 +155,8 @@ use_repulsion_params = [
 )
 @pytest.mark.parametrize("use_repulsion", use_repulsion_params)
 def test_lmp_with_ase(
-        kokkos: bool, use_cuaev: bool, precision: str, nbr: str, pbc: bool, device: str, num_tasks: int, use_repulsion: bool):
+        kokkos: bool, use_cuaev: bool, precision: str, nbr: str, pbc: bool, device: str, num_tasks: int,
+        use_repulsion: bool):
     # SKIP: compiled kokkos only work on Ampere GPUs
     SM = torch.cuda.get_device_capability(0)
     SM = int(f'{SM[0]}{SM[1]}')
@@ -99,7 +189,7 @@ def test_lmp_with_ase(
         var_dict["newton_pair"] = "on"
 
     # run lammps
-    lmprunner = lammps_ani.utils.LammpsRunner(LAMMPS_PATH, "in.lammps", var_dict, kokkos, num_tasks)
+    lmprunner = LammpsRunner(LAMMPS_PATH, "in.lammps", var_dict, kokkos, num_tasks)
     lmp_dump = lmprunner.run()
 
     # setup ase calculator
@@ -125,8 +215,8 @@ def test_lmp_with_ase(
     calculator = torchani.ase.Calculator(ani2x.to(dtype).to(device))
 
     # run ase
-    aserunner = lammps_ani.utils.AseRunner("water-0.8nm.pdb", calculator=calculator, pbc=pbc)
+    aserunner = AseRunner("water-0.8nm.pdb", calculator=calculator, pbc=pbc)
     ase_traj = aserunner.run()
 
     # check result
-    lammps_ani.utils.compare_lmp_ase(lmp_dump, ase_traj, high_prec=use_double)
+    compare_lmp_ase(lmp_dump, ase_traj, high_prec=use_double)

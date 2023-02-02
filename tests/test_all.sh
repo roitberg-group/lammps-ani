af3039fe542d21e5896b10ad66948fef920c27b9
richard <yueyericardo@gmail.com>
2023-02-02 14:20:01 -0500

    quick save

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -4,22 +4,22 @@ set -ex
 NUM_GPUs=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
 # test_model
-${LAMMPS_ANI_ROOT}/build/lammps_ani/ani_csrc/test_model ani2x.pt cpu double
-${LAMMPS_ANI_ROOT}/build/lammps_ani/ani_csrc/test_model ani2x.pt cpu single
+${LAMMPS_ANI_ROOT}/build/src/ani_csrc/test_model ani2x.pt cpu double
+${LAMMPS_ANI_ROOT}/build/src/ani_csrc/test_model ani2x.pt cpu single
 if [ $NUM_GPUs -gt 0 ]; then
-    ${LAMMPS_ANI_ROOT}/build/lammps_ani/ani_csrc/test_model ani2x.pt cuda double
-    ${LAMMPS_ANI_ROOT}/build/lammps_ani/ani_csrc/test_model ani2x.pt cuda single
+    ${LAMMPS_ANI_ROOT}/build/src/ani_csrc/test_model ani2x.pt cuda double
+    ${LAMMPS_ANI_ROOT}/build/src/ani_csrc/test_model ani2x.pt cuda single
 fi
 
 # test_ani2x_nocuaev_double
 cd ./lammps-unittest/test_ani2x_nocuaev_double_half && ./test.sh
-cd run && ./run.sh && cd ../../
+cd run && ./run.sh && cd ../../../
 
 cd ./lammps-unittest/test_ani2x_nocuaev_single_half && ./test.sh
-cd run && ./run.sh && cd ../../
+cd run && ./run.sh && cd ../../../
 
 cd ./lammps-unittest/test_ani2x_cuaev_single_half && ./test.sh
-cd run && ./run.sh && cd ../../
+cd run && ./run.sh && cd ../../../
 
 cd ./lammps-unittest/test_ani2x_cuaev_single_full && ./test.sh
-cd run && ./run.sh && cd ../../
+cd run && ./run.sh && cd ../../../

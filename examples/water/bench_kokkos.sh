28d44868e13ed3baf9caee0e0025e413077b1761
richard <yueyericardo@gmail.com>
2023-02-03 15:31:12 -0500

    quick save

--- examples/water/bench_kokkos.sh
+++ examples/water/bench_kokkos.sh
@@ -12,9 +12,3 @@ LAMMPS_ANI_PROFILING=1 mpirun -np ${NUM_GPUS} ${LAMMPS_ROOT}/build/lmp_mpi \
     -k on g ${NUM_GPUS} -sf kk -pk kokkos gpu/aware on \
     -var newton_pair on -var num_models ${NUM_MODELS} -var datafile ${DIR}/water.data \
     -log ${DIR}/log-${TIMESTAMP}-kokkos-models_${NUM_MODELS}-gpus_${NUM_GPUS}.log -in in.lammps
-
-
-
-
-
-

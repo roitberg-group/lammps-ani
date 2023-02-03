e4ec34f802e502211929814b921fd8ff3f6351b7
richard <yueyericardo@gmail.com>
2023-02-03 16:26:50 -0500

    cleanup 3

--- examples/water/bench_nokokkos.sh
+++ examples/water/bench_nokokkos.sh
@@ -3,9 +3,9 @@ set -ex
 TIMESTAMP=`date +%F-%H%M`
 
 # 20k or 300k
-DIR=300k
+DIR=20k
 NUM_MODELS=1
-NUM_GPUS=8
+NUM_GPUS=1
 
 mpirun -np ${NUM_GPUS} ${LAMMPS_ROOT}/build/lmp_mpi \
     -var newton_pair off -var num_models ${NUM_MODELS} -var datafile ${DIR}/water.data \

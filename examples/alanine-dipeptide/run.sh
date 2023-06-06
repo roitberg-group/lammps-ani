cbe5de642ccbfd93bf63089df9afa69018d5d4fd
richard <yueyericardo@gmail.com>
2023-06-06 14:40:58 -0400

    run name

--- examples/alanine-dipeptide/run.sh
+++ examples/alanine-dipeptide/run.sh
@@ -7,6 +7,7 @@ TIMESTAMP=`date +%F-%H%M%S`
 
 ################################# Configure here ################################# 
 
+RUN_NAME=run
 LAMMPS_INPUT=in.lammps
 RUN_KOKKOS=yes
 NUM_GPUS=1
@@ -28,10 +29,10 @@ if  [[ $RUN_KOKKOS == "yes" ]]; then
     LAMMPS_ANI_PROFILING=1 mpirun -np ${NUM_GPUS} ${LAMMPS_ROOT}/build/lmp_mpi \
         -k on g ${NUM_GPUS} -sf kk -pk kokkos gpu/aware on \
         -var newton_pair on -var num_models ${NUM_MODELS} -var datafile ${DATA_FILE} -var timestamp ${TIMESTAMP} -var modelfile ${MODEL_FILE} -var timestep ${TIMESTEP}\
-        -log logs/${TIMESTAMP}-kokkos-models_${NUM_MODELS}-gpus_${NUM_GPUS}.log -in ${LAMMPS_INPUT}
+        -log logs/${TIMESTAMP}-kokkos-models_${NUM_MODELS}-gpus_${NUM_GPUS}-${RUN_NAME}.log -in ${LAMMPS_INPUT}
 else
     # run without kokkos
     mpirun -np ${NUM_GPUS} ${LAMMPS_ROOT}/build/lmp_mpi \
         -var newton_pair off -var num_models ${NUM_MODELS} -var datafile ${DATA_FILE} -var timestamp ${TIMESTAMP} -var modelfile ${MODEL_FILE} -var timestep ${TIMESTEP}\
-        -log logs/${TIMESTAMP}-models_${NUM_MODELS}-gpus_${NUM_GPUS}.log -in ${LAMMPS_INPUT}
+        -log logs/${TIMESTAMP}-models_${NUM_MODELS}-gpus_${NUM_GPUS}-${RUN_NAME}.log -in ${LAMMPS_INPUT}
 fi

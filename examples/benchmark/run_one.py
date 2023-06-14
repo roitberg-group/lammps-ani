3808245033e96d46cbce628606c8c754f18330eb
richard <yueyericardo@gmail.com>
2023-06-14 17:24:03 -0400

    polish run_one

--- examples/benchmark/run_one.py
+++ examples/benchmark/run_one.py
@@ -18,7 +18,9 @@ class LammpsRunner:
         run_name: str = "run",
         allow_tf32: bool = False,
     ):
-        # additional variables
+        """
+        Initialize LammpsRunner.
+        """
         var_dict["newton_pair"] = "on" if kokkos else "off"
         var_dict["ani_device"] = "cuda"
         var_dict["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
@@ -33,51 +35,71 @@ class LammpsRunner:
 
         # Create logs directory and logfile name
         os.makedirs(log_dir, exist_ok=True)
-        logfile = f"{log_dir}/{var_dict['timestamp']}-{'kokkos-' if kokkos == 'yes' else ''}models_{var_dict['ani_num_models']}-gpus_{num_gpus}-{run_name}.log"
-        env_vars = ""
-        if kokkos:
-            env_vars += "LAMMPS_ANI_PROFILING=1 "
-        if allow_tf32:
-            env_vars += "LAMMPS_ANI_ALLOW_TF32=1 "
+        logfile = os.path.join(
+            log_dir,
+            f"{var_dict['timestamp']}-{'kokkos-' if kokkos else ''}models_{var_dict['ani_num_models']}-gpus_{num_gpus}-{run_name}.log",
+        )
 
-        run_commands = (
-            f"{env_vars} mpirun -np {num_gpus} {lmp} "
+        self.run_commands = (
+            f"mpirun -np {num_gpus} {lmp} "
             f"{var_commands} {kokkos_commands} "
             f"-in {input_file} -log {logfile}"
         )
-        print(f"Run with command:\n{run_commands}")
-        self.run_commands = run_commands
+
+        add_env_vars = {}  # additional environment variables
+        if kokkos:
+            add_env_vars["LAMMPS_ANI_PROFILING"] = "1"
+        if allow_tf32:
+            add_env_vars["LAMMPS_ANI_ALLOW_TF32"] = "1"
+
+        env_vars = ""
+        for k, v in add_env_vars.items():
+            env_vars += f"{k}={v} "
+
+        self.run_commands = env_vars + self.run_commands
+        print(f"Run with command:\n{self.run_commands}")
 
     def run(self):
-        stdout = subprocess.run(
-            self.run_commands,
-            shell=True,
-            check=True,
-        )
+        """
+        Run the LAMMPS simulation.
+        """
+        try:
+            subprocess.run(
+                self.run_commands,
+                shell=True,
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred while running LAMMPS: {e}")
 
 
-# run parameters
-run_name = "run_nvt"
-num_gpus = 1
-kokkos = True
-log_dir = "logs"
-input_file = "in.lammps"
-allow_tf32 = False
+if __name__ == "__main__":
+    # run parameters
+    run_name = "run_nvt"
+    num_gpus = 1
+    kokkos = True
+    log_dir = "logs"
+    input_file = "in.lammps"
+    allow_tf32 = True
 
-# variables for lammps input file
-var_dict = {
-    # configuration
-    "data_file": "data/water-150k.data",
-    "timestep": 0.5,
-    "run_steps": 1000,
-    # ani variables
-    "ani_model_file": os.getenv("LAMMPS_ANI_ROOT") + "/tests/ani2x.pt",
-    "ani_num_models": -1,  # -1 means use all models
-    "ani_aev": "cuaev",
-    "ani_neighbor": "full",
-    "ani_precision": "single",
-}
+    # variables for lammps input file
+    var_dict = {
+        # configuration
+        "data_file": "data/water-150k.data",
+        "timestep": 0.5,
+        "run_steps": 1000,
+        # ani variables
+        "ani_model_file": os.getenv("LAMMPS_ANI_ROOT") + "/tests/ani2x.pt",
+        "ani_num_models": 1,  # -1 means use all models
+        "ani_aev": "cuaev",  # switch to pyaev if run npt
+        "ani_neighbor": "full",
+        "ani_precision": "single",
+    }
 
-LammpsRunner(
-    LAMMPS_PATH, input_file, var_dict, kokkos, num_gpus, log_dir, run_name, allow_tf32
-)
+    lmp_runner = LammpsRunner(
+        LAMMPS_PATH, input_file, var_dict, kokkos, num_gpus, log_dir, run_name, allow_tf32
+    )
+    lmp_runner.run()
+
+    # warmup!
+    # benchmark tf32 again

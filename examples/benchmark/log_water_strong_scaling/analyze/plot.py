ca1043f45fd0f9aff9cd70df5447eea8c40eb368
richard <yueyericardo@gmail.com>
2023-06-26 15:58:56 -0400

    strong scaling plot

--- examples/benchmark/log_water_strong_scaling/analyze/plot.py
+++ examples/benchmark/log_water_strong_scaling/analyze/plot.py
@@ -1,58 +1,76 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+def human_format(num):
+    # Function to format numbers to human readable format
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '%.0f%s' % (num, ['', 'K', 'M', 'B', 'T', 'P'][magnitude])
 
 # Load the strong scaling data from the CSV file
 df_strong_scaling = pd.read_csv('../all.csv')  # update this with the path to your CSV file
 
-# Separate the data by atom count
-df_300k = df_strong_scaling[df_strong_scaling['atoms'] == 300003]
-df_1m = df_strong_scaling[df_strong_scaling['atoms'] == 1000002]
-df_10m = df_strong_scaling[df_strong_scaling['atoms'] == 10000002]
+# Get all unique atom counts
+atom_counts = df_strong_scaling['atoms'].unique()
 
-# Define the color scheme
-colors = ['red', 'blue', 'green']
+# Raise an error if there are more than 10 unique atom counts
+if len(atom_counts) > 10:
+    raise ValueError("Number of unique atom counts exceeds 10")
 
-fig, axs = plt.subplots(2, 1, figsize=(5, 7), dpi=150)
+# Define a list of 10 colors
+colors = ['red', 'blue', 'green', 'purple', 'orange', 'brown', 'pink', 'gray', 'olive', 'cyan']
 
-# First subplot for Timesteps/s vs Number of GPUs
-axs[0].plot(df_300k['num_gpus'], df_300k['timesteps/s'], 'o-', color=colors[0], label='300k atoms')
-axs[0].plot(df_1m['num_gpus'], df_1m['timesteps/s'], 'o-', color=colors[1], label='1M atoms')
-axs[0].plot(df_10m['num_gpus'], df_10m['timesteps/s'], 'o-', color=colors[2], label='10M atoms')
+fig, axs = plt.subplots(2, 1, figsize=(5.5, 6), dpi=150)
 
-# Add labels and title
+# Iterate over the unique atom counts
+for i, atom_count in enumerate(atom_counts):
+    # Filter the data for the current atom count
+    df_atom = df_strong_scaling[df_strong_scaling['atoms'] == atom_count]
+
+    # Plot the Timesteps/s vs Number of GPUs
+    axs[0].plot(df_atom['num_gpus'], df_atom['timesteps/s'], 'o-', color=colors[i], label=f'{human_format(atom_count)} atoms')
+
+    # Plot the Matom_step/s vs Number of GPUs
+    axs[1].plot(df_atom['num_gpus'], df_atom['Matom_step/s'], 'o-', color=colors[i], label=f'{human_format(atom_count)} atoms')
+
+
+# Specify the range of your x-axis (modify as needed)
+x_min = 0
+x_max = int(np.log2(df_atom['num_gpus'].max()))
+
+# Generate the ticks
+x_ticks = np.logspace(x_min, x_max, base=2, num=x_max-x_min+1)
+
+# Configure the first subplot
 axs[0].set_xlabel('Number of GPUs')
 axs[0].set_ylabel('Timesteps/s')
-axs[0].set_title('Strong Scaling: Timesteps/s vs Number of GPUs')
-
-# Set axes to log scale
-axs[0].set_xscale('log')
+axs[0].set_title('Strong Scaling Speed Timesteps/s')
+axs[0].set_xscale('log', base=2)
+axs[0].get_xaxis().set_major_formatter(ticker.FuncFormatter(lambda x, _: '{:.0f}'.format(x)))
 axs[0].set_yscale('log')
-
-# Add a grid
+axs[0].tick_params(axis='y', which='major', width=1, length=6)
+axs[0].tick_params(axis='y', which='minor', width=1, length=3)
 axs[0].grid(True)
+axs[0].set_xticks(x_ticks)
+axs[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-# Add a legend
-axs[0].legend()
-
-# Second subplot for Matoms_step/s vs Number of GPUs
-axs[1].plot(df_300k['num_gpus'], df_300k['Matoms_step/s'], 'o-', color=colors[0], label='300k atoms')
-axs[1].plot(df_1m['num_gpus'], df_1m['Matoms_step/s'], 'o-', color=colors[1], label='1M atoms')
-axs[1].plot(df_10m['num_gpus'], df_10m['Matoms_step/s'], 'o-', color=colors[2], label='10M atoms')
-
-# Add labels and title
+# Configure the second subplot
 axs[1].set_xlabel('Number of GPUs')
-axs[1].set_ylabel('Matoms_step/s')
-axs[1].set_title('Strong Scaling: Matoms_step/s vs Number of GPUs')
-
-# Set axes to log scale
-axs[1].set_xscale('log')
+axs[1].set_ylabel('Matom_step/s')
+axs[1].set_title('Strong Scaling Throughput Matom_step/s')
+axs[1].set_xscale('log', base=2)
+axs[1].get_xaxis().set_major_formatter(ticker.FuncFormatter(lambda x, _: '{:.0f}'.format(x)))
 axs[1].set_yscale('log')
-
-# Add a grid
+axs[1].tick_params(axis='y', which='major', width=1, length=6)
+axs[1].tick_params(axis='y', which='minor', width=1, length=3)
 axs[1].grid(True)
-
-# Add a legend
-axs[1].legend()
+axs[1].set_xticks(x_ticks)
+axs[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 plt.tight_layout()
 plt.show()

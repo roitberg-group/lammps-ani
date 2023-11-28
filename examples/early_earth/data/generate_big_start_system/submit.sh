0c53dbcb9c8443244ca2120600d2bb8e0d3ea0bd
richard <yueyericardo@gmail.com>
2023-11-28 12:20:13 -0500

    Clean Up Early Earth

--- examples/early_earth/data/generate_big_start_system/submit.sh
+++ examples/early_earth/data/generate_big_start_system/submit.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=40gb
-#SBATCH -t 18:00:00
-#SBATCH --qos=roitberg-b
+#SBATCH --mem=80gb
+#SBATCH -t 120:00:00
+#SBATCH --qos=roitberg
 #SBATCH --account=roitberg
 #SBATCH -o %j.out
 
@@ -14,4 +14,4 @@ module load cuda/11.4.3 gcc/9.3.0 openmpi/4.0.5 cmake/3.21.3 git/2.30.1 netcdf/4
 source $(conda info --base)/etc/profile.d/conda.sh
 # conda activate /blue/roitberg/jinzexue/conda-envs/openmm 
 echo $(which python)
-packmol < mixture_228000.inp 
+packmol < mixture_22800000.inp 

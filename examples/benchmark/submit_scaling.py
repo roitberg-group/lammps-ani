0604139755855e4455b2f23d4fba0657b2e055da
richard <yueyericardo@gmail.com>
2023-12-01 14:16:14 -0500

    save

--- examples/benchmark/submit_scaling.py
+++ examples/benchmark/submit_scaling.py
@@ -49,7 +49,7 @@ def setup_and_run_job(num_gpus, data_file, job_name, submit=False, weak_scaling=
         qos="roitberg",
         account="roitberg",
         gres=gres,
-        mem_per_cpu="30gb",
+        mem_per_cpu="100gb",
         time="120:00:00",
         output=output_filename,
     )
@@ -76,7 +76,7 @@ def setup_and_run_job(num_gpus, data_file, job_name, submit=False, weak_scaling=
         # run the job commands
         # "python run_one.py --help",
         # --allow_tf32
-        f"python run_one.py {data_file} --input_file=in.lammps --replicate='{replicate}' --kokkos --num_gpus={num_gpus} --run_steps=5000 --run_name='run' --log_dir={log_dir} --ani_num_models=8 --run"
+        f"python run_one.py {data_file} --input_file=in.lammps --replicate='{replicate}' --kokkos --num_gpus={num_gpus} --run_steps=5000 --run_name='run' --log_dir={log_dir} --run"
     ]
     commands = "\n".join(commands)
     if submit:
